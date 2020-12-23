4de148ab373233e2a87df02528b1c178ce5166ab
Anand J <anandj@cs.toronto.edu>
2020-12-23 18:38:42 -0500

    Add comments in the sample code

--- HelloTrill/Program.cs
+++ HelloTrill/Program.cs
@@ -9,18 +9,24 @@ namespace HelloTrill
     {
         static void Main(string[] args)
         {
-            var list = new List<int> {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+            var SIZE = 1000;                                    // Size of the data set
+            var list = new List<int>();                         // A list for storing the data points
+            for (int i = 0; i < SIZE; i++)
+            {
+                list.Add(i);                                    // Populate the list with dummy data
+            }
             var stream = list
-                    .ToObservable()
-                    .ToTemporalStreamable(e => e, e => e + 1)
-                ;
+                    .ToObservable()                             // Convert the data list to an Observable first 
+                    .ToTemporalStreamable(e => e, e => e + 1)   // Then convert to Trill temporal stream;
+                ;                                               // Each event has an integer payload 'n'
+                                                                // and an interval of [n, n+1)
             var obs = stream
-                    .Select(e => e + 1)
-                ;
+                    .Select(e => e + 1)                         // Set transformations on the stream.
+                ;                                               // In this case, Adding 1 to each payload using Select
             obs
-                .ToStreamEventObservable()
-                .Where(e => e.IsData)
-                .ForEach(e => { Console.WriteLine(e); })
+                .ToStreamEventObservable()                      // Convert back to Observable (of StreamEvents)
+                .Where(e => e.IsData)                           // Only pick data events from the stream
+                .ForEach(e => { Console.WriteLine(e); })        // Print the events to the console
                 ;
         }
     }

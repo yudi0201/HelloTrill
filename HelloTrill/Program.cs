4d58daee3e6f86d0caae400fce9173330fba340e
Anand J <anandj@cs.toronto.edu>
2020-12-23 18:47:07 -0500

    More comments

--- HelloTrill/Program.cs
+++ HelloTrill/Program.cs
@@ -9,21 +9,43 @@ namespace HelloTrill
     {
         static void Main(string[] args)
         {
+            /**
+             * Generating synthetic data
+             */
             var SIZE = 1000;                                    // Size of the data set
-            var list = new List<int>();                         // A list for storing the data points
+            var listA = new List<int>();                        // A list for storing the data points
+            var listB = new List<int>();                        // Another list for storing the data points
             for (int i = 0; i < SIZE; i++)
             {
-                list.Add(i);                                    // Populate the list with dummy data
+                listA.Add(i);                                    // Populate listA with dummy data
+                listB.Add(i);                                    // Populate listB with dummy data
             }
-            var stream = list
+            
+            /**
+             * Creating lists created above to Trill streams
+             */
+            var streamA = listA                                 // Creating first stream from listA 
                     .ToObservable()                             // Convert the data list to an Observable first 
                     .ToTemporalStreamable(e => e, e => e + 1)   // Then convert to Trill temporal stream;
-                ;                                               // Each event has an integer payload 'n'
+                ;                                               // nth event in the stream has an integer payload 'n'
                                                                 // and an interval of [n, n+1)
-            var obs = stream
+            
+            var streamB = listB                                 // Creating streamB (not using yet) similar to streamA.
+                    .ToObservable()
+                    .ToTemporalStreamable(e => e, e => e + 1)
+                ;
+            
+            /**
+             * Define transformations on the stream(s) 
+             */
+            var result = streamA
                     .Select(e => e + 1)                         // Set transformations on the stream.
                 ;                                               // In this case, Adding 1 to each payload using Select
-            obs
+            
+            /**
+             * Print out the result
+             */
+            result
                 .ToStreamEventObservable()                      // Convert back to Observable (of StreamEvents)
                 .Where(e => e.IsData)                           // Only pick data events from the stream
                 .ForEach(e => { Console.WriteLine(e); })        // Print the events to the console
